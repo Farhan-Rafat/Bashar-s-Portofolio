325475e10e187bb2ef999fee419bbf24cb91a534
Farhan rafat <farhanrafatdev@gmail.com>
2024-09-04 21:47:58 +0600

    created homepage

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  darkMode: "selector",
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"],
+        madeCanvas: ["MADE Canvas"],
+      },
+      colors: {
+        primary: "#576665",
+        secondary: "#FFD6D6",
+        black: "#020506",
+        grey: "#ECECEC",
+        olivelight: "#576665",
+      },
+
+      fontSize: {
+        18: "1.125rem",
+        21: "1.313rem",
+        32: "2rem",
+        38: "2.375rem",
+      },
+
+      spacing: {
+        30: "1.875rem",
+        50: "3.125rem",
+        88: "5.5rem",
+        70: "4.375rem",
+      },
+    },
+  },
+  plugins: [],
+};
