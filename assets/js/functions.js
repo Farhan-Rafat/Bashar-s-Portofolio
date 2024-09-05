cb9bf1825ddf1d4b0681866921008eb86e8d6106
Farhan rafat <farhanrafatdev@gmail.com>
2024-09-05 20:49:58 +0600

    added testimonials page

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -11,3 +11,15 @@ function darkModeListener() {
 document
   .querySelector("input[type='checkbox']#dark-toggle")
   .addEventListener("click", darkModeListener);
+
+var stars = document.querySelectorAll(".star a");
+
+stars.forEach((item, index1) => {
+  item.addEventListener("click", () => {
+    stars.forEach((star, index2) => {
+      index1 >= index2
+        ? star.classList.add("active")
+        : star.classList.remove("active");
+    });
+  });
+});
